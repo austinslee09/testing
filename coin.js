d0d50017306db5b8f00b036433bbffe5db1b3ae3
Austin Lee <austinlee@usf.edu>
2025-09-07 12:47:34 -0400

    if statements

--- coin.js
+++ coin.js
@@ -1,8 +1,6 @@
 const body=document.getElementsByTagName("body") [0];
 
 function flip(coin){
-const hit=Math.round (math.random()*255);
-
-let heads= hit<127
-let tails= hit>127
+if(math.random()>.5)=heads;
+if(math.random()<.5)=tails;
 }
